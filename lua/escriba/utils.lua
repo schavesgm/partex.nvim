0c32024f0ab0bf135f54710bea91c71dcca603d6
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-09-07 11:24:46 +0100

    added utility functions, needed to filter some keymaps

---
+++ lua/escriba/utils.lua
@@ -0,0 +1,26 @@
+---Get a keymap table on a given mode, filtering by lhs keystroke
+---@param mode string #Neovim mode
+---@param lhs string  #Keystroke to filter
+local function get_keymap(mode, lhs)
+    local keymaps, found = vim.api.nvim_get_keymap(mode), nil
+    for _, keymap in ipairs(keymaps) do
+        if (keymap.lhs == lhs) then
+            found = keymap
+            break
+        end
+    end
+    return found
+end
+
+---Set a keymap from a keymap table
+---@param keymap table #Table containing the keymap definition
+local function set_keymap(keymap)
+    local rhs  = (keymap.expr == 0) and keymap.callback or keymap.expr
+    local opts = {buffer=keymap.buffer, silent=keymap.silent, remap=not keymap.noremap}
+    vim.keymap.set(keymap.mode, keymap.lhs, rhs, opts)
+end
+
+return {
+    get_keymap     = get_keymap,
+    set_keymap     = set_keymap,
+}
