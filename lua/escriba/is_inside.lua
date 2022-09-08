6317f4f54db35693f02a7a3988eb48f5a6c32af8
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-09-08 17:02:56 +0100

    improved performance of functions by avoiding complete loops and treesitter parsing

--- lua/escriba/is_inside.lua
+++ lua/escriba/is_inside.lua
@@ -3,51 +3,8 @@ local keywords = 'section\\|subsection\\|subsubsection\\|chapter\\|documentclass
 local regex_emptyline = vim.regex("\\(^$\\)\\|\\(^\\s\\+$\\)")
 local regex_keywdline = vim.regex(".*\\\\\\(" .. keywords .. "\\|begin\\).*")
 
----Check if a current line number is inside bounds
----@param lnum number #Line number to be checked; vim-like indexingz (1, $)
----@param bounds table #Table containing all the queried matches
-local function is_inside_bounds(lnum, bounds)
-    for _, limits in ipairs(bounds) do
-        local is_inside = (limits[1] <= lnum - 1) and (lnum - 1 <= limits[2])
-        if is_inside then return true end
-    end
-    return false
-end
-
----Get all lines matching a LaTeX query
----@param query_str string #Query string to be processed
----@return table #Table containing all lines child of the queried nodes, empty table if no matches
-local function get_lines_matching_query(query_str)
-    local bufnr = vim.api.nvim_get_current_buf()
-    local tree  = vim.treesitter.get_parser(bufnr, 'latex')
-    local query = vim.treesitter.parse_query('latex', query_str)
-
-    -- Get all the matches from the query
-    local matches = query:iter_matches(tree:parse()[1]:root(), bufnr)
-    if (matches == nil) then return {} end
-
-    local bounds = {}
-    for _, _, metadata in matches do
-        local content = metadata.content[1]
-        table.insert(bounds, {content[1], content[3]})
-    end
-    return bounds
-end
-
----Check if the current line belongs to any of the matchs of a given query
----@param lnum number #Line number to be checked; vim-like indexing (1, $)
----@param query string #Query string to be processed
-local function is_inside_query(lnum, query)
-    local bounds = get_lines_matching_query(query)
-    if next(bounds) == nil then return false end
-    return is_inside_bounds(lnum, bounds)
-end
-
----Check if the current line is inside a text node
----@param lnum number #Line number to be checked; vim-like indexing (1, $)
-local function is_inside_text(lnum)
-    return is_inside_query(lnum, [[((text) @lines (#offset! @lines))]])
-end
+-- Queries to be used in the analysis
+local queries = require"escriba.queries"
 
 ---Check if a line is treated as special: contains special keywords or is empty
 ---@param lnum number #Line number to be checked: vim-like indexing (1, $)
@@ -59,74 +16,80 @@ local function is_special_line(lnum)
     return (is_empty or is_keywr)
 end
 
+---Get all lines matching a LaTeX query
+---@param lnum number #Line number to be checked; vim-like indexing (1, $)
+---@param root table #Root of the syntax tree to be used in the analysis
+---@param query table #Query string to be processed
+local function check_is_inside_query(lnum, root, query)
+    local matches = query:iter_matches(root, vim.api.nvim_get_current_buf())
+    if (matches == nil) then return {} end
+    for _, _, metadata in matches do
+        local lbound, ubound = metadata.content[1][1], metadata.content[1][3]
+        local is_inside = (lbound <= lnum - 1) and (lnum - 1 <= ubound)
+        if is_inside then return true end
+    end
+    return false
+end
+
+---Check if the current line is inside a text node
+---@param lnum number #Line number to be checked; vim-like indexing (1, $)
+---@param root table #Root of the syntax tree to be used in the analysis
+local function is_inside_text(lnum, root)
+    return check_is_inside_query(lnum, root, queries.text_query)
+end
+
 ---Check if the current line is inside generic_environment, "document" is ignored
 ---@param lnum number #Line number to be checked; vim-like indexing (1, $)
-local function is_inside_generic_environment(lnum)
-    return is_inside_query(lnum,
-    [[
-        (
-            (generic_environment
-                begin: (begin
-                    name: (curly_group_text) @marker (#not-eq? @marker "{document}")
-                )
-            ) @lines (#offset! @lines)
-        )
-    ]])
+---@param root table #Root of the syntax tree to be used in the analysis
+local function is_inside_generic_environment(lnum, root)
+    return check_is_inside_query(lnum, root, queries.genv_query)
 end
 
 ---Check if the current line is inside a math_environment
 ---@param lnum number #Line number of be checked; vim-like indexing (1, $)
-local function is_inside_math_environment(lnum)
-    return is_inside_query(lnum, [[((math_environment) @lines (#offset! @lines))]])
+---@param root table #Root of the syntax tree to be used in the analysis
+local function is_inside_math_environment(lnum, root)
+    return check_is_inside_query(lnum, root, queries.menv_query)
 end
 
 ---Check if the current line is inside several special environments
 ---@param lnum number #Line number to be checked; vim-like indexing (1, $)
-local function is_inside_special_environment(lnum)
+---@param root table #Root of the syntax tree to be used in the analysis
+local function is_inside_special_environment(lnum, root)
     -- Check if the current line is inside a special environment
-    return is_inside_query(lnum,
-    [[
-        ((package_include) @lines (#offset! @lines))
-        ((author_declaration) @lines (#offset! @lines))
-        ((title_declaration) @lines (#offset! @lines))
-    ]])
+    return check_is_inside_query(lnum, root, queries.senv_query)
 end
 
 ---Check if the current line corresponds to an isolated command environment
 ---@param lnum number #Line number to be checked; vim-like indexing (1, $)
-local function is_isolated_command(lnum)
-    return not is_inside_query(lnum,
-        [[((text (generic_command) @lines (#offset! @lines)))]]
-    ) and not is_inside_query(lnum,
-        [[((inline_formula) @lines (#offset! @lines))]]
-    )
+---@param root table #Root of the syntax tree to be used in the analysis
+local function is_isolated_command(lnum, root)
+    return not check_is_inside_query(lnum, root, queries.tgen_query) and
+           not check_is_inside_query(lnum, root, queries.inlm_query)
 end
 
 ---Check if the current line is inside a paragraph
 ---@param lnum number #Line number to be checked: vim-like indexing (1, $)
-local function is_inside_paragraph(lnum)
-    if not is_inside_text(lnum) then return false end
+---@param root table #Root of the syntax tree to be used in the analysis
+local function is_inside_paragraph(lnum, root)
+    if not is_inside_text(lnum, root) then return false end
     local not_special_line = not is_special_line(lnum)
-    local not_inside_genv  = not is_inside_generic_environment(lnum)
-    local not_inside_menv  = not is_inside_math_environment(lnum)
-    local not_inside_senv  = not is_inside_special_environment(lnum)
+    local not_inside_genv  = not is_inside_generic_environment(lnum, root)
+    local not_inside_menv  = not is_inside_math_environment(lnum, root)
+    local not_inside_senv  = not is_inside_special_environment(lnum, root)
+    local above_conditions = not_special_line and not_inside_genv and not_inside_menv and not_inside_senv
 
-    if not_special_line and not_inside_genv and not_inside_menv and not_inside_senv then
-        local has_cmd = is_inside_query(lnum, [[((generic_command) @lines (#offset! @lines))]])
+    if above_conditions then
+        local has_cmd = check_is_inside_query(lnum, root, queries.gcmd_query)
         if not has_cmd then
             return true
         else
-            if not is_isolated_command(lnum) then return true end
+            if not is_isolated_command(lnum, root) then return true end
         end
     end
     return false
 end
 
 return {
-    is_inside_generic_environment = is_inside_generic_environment,
-    is_inside_math_environment    = is_inside_math_environment,
-    is_inside_special_environment = is_inside_special_environment,
-    is_special_line               = is_special_line,
-    is_isolated_command           = is_isolated_command,
-    is_inside_paragraph           = is_inside_paragraph
+    is_inside_paragraph = is_inside_paragraph
 }
