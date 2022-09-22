06a26ab70a4470d6f7c59a43787f07b41400b9cd
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-09-22 18:17:53 +0100

    refactor(rename): change plugin name

---
+++ plugin/partex.lua
@@ -0,0 +1,10 @@
+if 1 ~= vim.fn.has("nvim-0.7.0") then
+    vim.api.nvim_err_writeln "partex.nvim requires at least nvim-0.7.0."
+    return
+end
+
+-- If the plugin is loaded, then ignore this call
+if vim.g.loaded_partex == 1 then
+    return
+end
+vim.g.loaded_partex = 1
