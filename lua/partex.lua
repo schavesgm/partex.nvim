29fae9b61b7aa9b5c718916eac45d8354de25404
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-01-16 23:12:09 +0000

    fix(main): correct bug in latex autocommands

--- lua/partex.lua
+++ lua/partex.lua
@@ -1,11 +1,11 @@
 local M = {}
 
--- Expose some functions 
+-- Expose some functions
 M.actions = vim.tbl_deep_extend("force",
-    require"partex.actions",
+    require "partex.actions",
     {
-        get_all_bounds_required = require"partex.treesitter".get_all_bounds_required,
-        is_inside_paragraph     = require"partex.is_inside".is_inside_paragraph,
+        get_all_bounds_required = require "partex.treesitter".get_all_bounds_required,
+        is_inside_paragraph     = require "partex.is_inside".is_inside_paragraph,
     }
 )
 
@@ -29,8 +29,8 @@ local function format_document(opts)
     else
         textwidth = tonumber(opts.args)
     end
-    vim.api.nvim_buf_set_option(bufnr, 'textwidth',  textwidth)
-    vim.api.nvim_set_option_value('colorcolumn', '+1', {scope='local'})
+    vim.api.nvim_buf_set_option(bufnr, 'textwidth', textwidth)
+    vim.api.nvim_set_option_value('colorcolumn', '+1', { scope = 'local' })
     M.actions.act_over_each_paragraph("vipgq")
 end
 
@@ -60,31 +60,33 @@ M.setup = function(settings)
 
     -- Create the excommand if desired
     if M.config.create_excmd then
-        vim.api.nvim_create_user_command('FormatTex', format_document, {bang=true, nargs='?'})
+        vim.api.nvim_create_user_command('FormatTex', format_document, { bang = true, nargs = '?' })
     end
 
     -- Create the keybindings if desired
     if M.config.keymaps.set then
 
-        ---@param callback function: Callback function to execute after wrapping
-        local filetype_wrapper = function(callback)
-            if vim.bo.filetype ~= "tex" then
-                vim.notify("Filetype is not tex. Ignoring...", vim.log.levels.WARN)
-                return
-            end
-            return callback
-        end
+        local group = vim.api.nvim_create_augroup("PartexKeymaps", {clear=true})
+        vim.api.nvim_create_autocmd(
+            {"BufEnter", "BufWinEnter"},
+            {
+                pattern = "*.tex",
+                group = group,
+                callback = function()
+                    local keymaps = M.config.keymaps
 
-        local keymaps = M.config.keymaps
-        -- Set the keymaps to select paragraphs
-        vim.keymap.set("o", keymaps.operator.select_inside,  filetype_wrapper(M.actions.select_inside_paragraph))
-        vim.keymap.set("n", keymaps.normal.select_inside,    filetype_wrapper(M.actions.select_inside_paragraph))
+                    -- Set the keymaps to select paragraphs
+                    vim.keymap.set("o", keymaps.operator.select_inside, M.actions.select_inside_paragraph)
+                    vim.keymap.set("n", keymaps.normal.select_inside, M.actions.select_inside_paragraph)
 
-        -- Set keymap to move to the following paragraph
-        vim.keymap.set("n", keymaps.normal.move_to_next,  filetype_wrapper(M.actions.move_to_next_paragraph))
-        vim.keymap.set("n", keymaps.normal.move_to_prev,  filetype_wrapper(M.actions.move_to_previous_paragraph))
-        vim.keymap.set("n", keymaps.normal.move_to_start, filetype_wrapper(M.actions.move_to_paragraph_start))
-        vim.keymap.set("n", keymaps.normal.move_to_end,   filetype_wrapper(M.actions.move_to_paragraph_end))
+                    -- Set keymap to move to the following paragraph
+                    vim.keymap.set("n", keymaps.normal.move_to_next, M.actions.move_to_next_paragraph)
+                    vim.keymap.set("n", keymaps.normal.move_to_prev, M.actions.move_to_previous_paragraph)
+                    vim.keymap.set("n", keymaps.normal.move_to_start, M.actions.move_to_paragraph_start)
+                    vim.keymap.set("n", keymaps.normal.move_to_end, M.actions.move_to_paragraph_end)
+                end
+            }
+        )
     end
 end
 
